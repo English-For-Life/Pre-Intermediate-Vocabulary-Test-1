bc03ea723c376d9f1fdf43df54833a7c17a83824
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-02 12:23:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Excuse me. I'm a(n) ...... in this neighbourhood. Can you tell me what number bus will take me to the town center, please? I want to do some shopping.",
+        options:["stranger", "immigrant", "tourist", "customer", "foreigner"],
+        answer: 0
+    },
+    {
+        q:"How much must I ...... a good second-hand car?",
+        options:["buy from", "pay for", "borrow from", "own for", "lend to"],
+        answer: 1
+    },
+    {
+        q:"It's getting quite ...... . I think it's going to rain soon.",
+        options:["foggy", "cold", "cloudy", "hot", "windy"],
+        answer: 2
+    },
+    {
+        q:"The ...... today is full of the news of yesterday's concert.",
+        options:["journalism", "literature", "reporter", "press", "publisher"],
+        answer: 3
+    },
+    {
+        q:"It's a very ...... picture. I laugh at it all the time.",
+        options:["laughing", "smiling", "joking", "serious", "funny"],
+        answer: 4
+    },
+    {
+        q:"Nobody gave him a hand. He did it on his ......   .",
+        options:["own", "lonely", "alone", "self", "single"],
+        answer: 0
+    },
+    {
+        q:"He received another letter from the ...... demanding more money.",
+        options:["robber", "blackmailer", "pickpocket", "thief", "burglar"],
+        answer: 1
+    },
+    {
+        q:"'How much is the ...... ?', she asked the bus conductor.",
+        options:["price", "cost", "fare", "prize", "fee"],
+        answer: 2
+    },
+    {
+        q:"Come to Turkland, and ...... all these places where history lives on.",
+        options:["inform", "define", "memorize", "discover", "describe"],
+        answer: 3
+    },
+    {
+        q:"There are a lot more television ...... today than there were twenty years ago.",
+        options:["sightseers", "spectators", "visitors", "onlookers", "viewers"],
+        answer: 4
+    }
+
+
+
+]
